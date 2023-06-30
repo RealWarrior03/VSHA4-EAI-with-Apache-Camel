c70ae4ee1f217a6959dc40fdec52725873a1824d
dbruchwald <d.bruchwald@campus.tu-berlin.de>
2023-06-30 09:43:32 +0200

    WOS message transformer implemented (not sure if the result should be a string or a message object)
    Expanded WebOrderSystem skeleton

---
+++ src/main/java/WOSInputTransformer.java
@@ -0,0 +1,20 @@
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class WOSInputTransformer implements Processor {
+
+    //TODO should we forward a String or a Message?
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String[] parts = exchange.getIn().getBody(String.class).split(", ");
+        String firstName = parts[0];
+        String lastName = parts[1];
+        String nmbOrdSurfBoards = parts[2];
+        String nmbOrdDivingSuits = parts[3];
+        String customerID = parts[4];
+
+        exchange.getIn().setBody(customerID + ", " + firstName + ", " + lastName + ", " + "null" + nmbOrdDivingSuits + ", "
+                + nmbOrdSurfBoards + ", " + "null" + ", " + "null");
+
+    }
+}
--- src/main/java/WebOrderSystem.java
+++ src/main/java/WebOrderSystem.java
@@ -7,16 +7,44 @@ public class WebOrderSystem {
     private static final String DESTINATION_FOLDER = "src/main/outputfiles/webordersystemoutput";
 
 
+    /*
+    INPUT:
+    a file containing multiple orders seperated by \n
+    order format:
+    FirstName, LastName, Number of ordered surfboards, Number of ordered diving suits, Customer-ID
+
+     OUTPUT:
+     none
+     */
+
     public static void main(String[] args) throws Exception {
         String filename = args[1];
+
+        //TODO add activemq stuff
+
         CamelContext camelContext = new DefaultCamelContext();
         camelContext.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("file://" + SOURCE_FOLDER + "?noop=true");
+                from("file://" + SOURCE_FOLDER + "?noop=true")
+                    .split(body().tokenize("\n"))
+                    .process(new WOSInputTransformer()) //transformWOS
+                    .enrich() //enrich Message
+                    .to();  //pubsub channel
             }
         });
         camelContext.start();
         camelContext.stop();
     }
 }
+
+/*
+o CustomerID
+o FirstName
+o LastName
+o OverallItems (Number of all items in order)
+o NumberOfDivingSuits
+o NumberOfSurfboards o OrderID
+o Valid
+o validationResult
+ */
