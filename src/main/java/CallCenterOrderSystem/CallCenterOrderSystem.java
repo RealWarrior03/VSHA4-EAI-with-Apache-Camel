114826f716e377ef58f1051a7bf7d5b4931c703f
hkrieger <henry.krieger@campus.tu-berlin.de>
2023-07-13 14:34:20 +0200

    fixed CCOS changes, updated readme

--- src/main/java/CallCenterOrderSystem/CallCenterOrderSystem.java
+++ src/main/java/CallCenterOrderSystem/CallCenterOrderSystem.java
@@ -64,16 +64,16 @@ public class CallCenterOrderSystem {
                                 System.out.println("Content of the OrderMessage Object");
                                 System.out.println(content.toString());
                             })
-                            .to("activemq:queue:orderIDGenIn"); //to queue channel TODO might be incorrectly implemented
-                            //.transform(body().append("\n"))
-                            //.to("file:" + DESTINATION_FOLDER + "?fileName=callcenterordersystemoutput.txt&noop=true&fileExist=Append"); //only for debugging
+                            .to("activemq:queue:orderIDGenIn") //to queue channel TODO might be incorrectly implemented
+                            .transform(body().append("\n"))
+                            .to("file:" + DESTINATION_FOLDER + "?fileName=callcenterordersystemoutput.txt&noop=true&fileExist=Append"); //only for debugging
                 }
             });
             camelContext.start();
 
             //camelContext.createProducerTemplate().sendBody("direct:start", "Peter, Parker, 2, 0, 1");
 
-            Thread.sleep(500000);
+            Thread.sleep(5000);
 
             camelContext.stop();
 
